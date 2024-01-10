3b980ee7222efc1faab403ae39e5419b633f251e
Terêncio <terencio@MacBook-Pro-van-MacbookPro.local>
2024-01-10 13:48:27 +0100

    Update add modal

--- js/script.js
+++ js/script.js
@@ -15,3 +15,32 @@ themeToggler.addEventListener('click',()=>{
     themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
     themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
 });
+
+/*****************
+ Modal script
+ * ***********/
+const open_modal_buttons = document.querySelectorAll('.openModal');
+const close_modal_buttons = document.querySelectorAll('.closeModal');
+
+open_modal_buttons.forEach(open_modal => {
+    open_modal.addEventListener('click',()=>{
+        const modalId = open_modal.getAttribute('data-target');
+        const modal_container = document.querySelector(modalId);
+        modal_container.classList.add('show')
+    });
+});
+
+close_modal_buttons.forEach(close_modal=>{
+    close_modal.addEventListener('click',()=>{
+        const modalId = close_modal.getAttribute('data-target');
+        const modal_container = document.querySelector(modalId);
+        modal_container.classList.remove('show')
+    }); 
+}); 
+
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal-container')) {
+        event.target.classList.remove('show');
+    }
+}    
+
